533dd2d65b1a6e10f501c4245e22cb11a4585d69
chechia <chechiachang999@gmail.com>
2022-09-15 12:45:12 +0800

    FEATURE: add checkov job to github action

--- azure/_poc/container_registry_module_remote/registry.tf
+++ azure/_poc/container_registry_module_remote/registry.tf
@@ -5,7 +5,8 @@ locals {
 }
 
 module "test" {
-  source = "git::ssh://git@github.com/chechiachang/terraform-30-days.git//azure/modules/container_registry?ref=v0.0.1"
+  //source = "git::ssh://git@github.com/chechiachang/terraform-30-days.git//azure/modules/container_registry?ref=v0.0.1"
+  source = "../../..//azure/modules/container_registry"
 
   registry_name                 = "chechiatest"
   resource_group_name           = local.resource_group_name
@@ -16,7 +17,8 @@ module "test" {
 module "registry" {
   for_each = toset(local.environments) # convert tuple to set of string
 
-  source = "git::ssh://git@github.com/chechiachang/terraform-30-days.git//azure/modules/container_registry?ref=v0.0.1"
+  //source = "git::ssh://git@github.com/chechiachang/terraform-30-days.git//azure/modules/container_registry?ref=v0.0.1"
+  source = "../../..//azure/modules/container_registry"
 
   registry_name                 = "chechia${each.value}"
   resource_group_name           = local.resource_group_name
